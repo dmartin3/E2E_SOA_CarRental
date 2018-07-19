fac0cf91020f00b2e43a3fbf64bc2c0c2b8788f1
Martin <Dan.Martin@mrisoftware.com>
2018-07-19 14:23:54 -0400

    Fixed up connection strings and integration tests

--- CarRental.Data.Tests/DataLayerTests.cs
+++ CarRental.Data.Tests/DataLayerTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using CarRental.Business.Bootstrapper;
 using CarRental.Business.Entities;
--- CarRental.ServiceHost.Console/Program.cs
+++ CarRental.ServiceHost.Console/Program.cs
@@ -1,9 +1,19 @@
-﻿using SM = System.ServiceModel;
+﻿using System;
+using CarRental.Business.Managers.Managers;
+using SM = System.ServiceModel;
 
 namespace CarRental.ServiceHost {
   class Program {
     static void Main(string[] args) {
+      Console.WriteLine("Starting up services...");
+      Console.WriteLine("");
 
+      var host = new SM.ServiceHost(typeof(InventoryManager));
+      host.Open();
+
+      Console.WriteLine("");
+      Console.WriteLine("Press [Enter] to exit.");
+      Console.ReadLine();
     }
   }
 }
