802226e7a1700e05845d7671ba006dde5fed8c3e
Martin <Dan.Martin@mrisoftware.com>
2018-07-23 18:04:39 -0400

    Began adding webapis

--- CarRental.Web/Binding/ViewModels/AccountLoginViewModel.js
+++ CarRental.Web/Binding/ViewModels/AccountLoginViewModel.js
@@ -1,32 +1,39 @@
-﻿appMainModule.controller("AccountLoginViewModel",
-  function ($scope, $http, viewModelHelper, validator) {
-    $scope.viewModelHelper = viewModelHelper;
-    $scope.accountModel = new CarRental.AccountLoginModel();
-    $scope.returnUrl = '';
+﻿appMainModule.controller("AccountLoginViewModel", function ($scope, $http, $location, viewModelHelper, validator) {
 
-    var accountModelRules = [];
+  $scope.viewModelHelper = viewModelHelper;
+  $scope.accountModel = new CarRental.AccountLoginModel();
+  $scope.returnUrl = '';
 
-    var setupRules = function () {
-      accountModelRules.push(new validator.PropertyRule("LoginEmail",
-        {
-          required: { message: "Login is required" },
-          email: { message: "Login must be in email format" }
-        }));
-      accountModelRules.push(new validator.PropertyRule("Password",
-        {
-          required: { message: "Password is required" },
-          minLength: { message: "Password must be at least 6 characters", params: 6 }
-        }));
+  var accountModelRules = [];
+
+  var setupRules = function () {
+    accountModelRules.push(new validator.PropertyRule("LoginEmail", {
+      required: { message: "Login is required" }
+    }));
+    accountModelRules.push(new validator.PropertyRule("Password", {
+      required: { message: "Password is required" },
+      minLength: { message: "Password must be at least 6 characters", params: 6 }
+    }));
+  }
+
+  $scope.login = function () {
+    validator.ValidateModel($scope.accountModel, accountModelRules);
+    viewModelHelper.modelIsValid = $scope.accountModel.isValid;
+    viewModelHelper.modelErrors = $scope.accountModel.errors;
+    if (viewModelHelper.modelIsValid) {
+      viewModelHelper.apiPost('api/account/login', $scope.accountModel,
+        function (result) {
+          if ($scope.returnUrl != '' && $scope.returnUrl.length > 1) {
+            window.location.href = CarRental.rootPath + $scope.returnUrl.substring(1);
+          }
+          else {
+            window.location.href = CarRental.rootPath;
+          }
+        });
     }
-
-    $scope.login = function () {
-      validator.ValidateModel($scope.accountModel, accountModelRules);
-      viewModelHelper.modelIsValue = $scope.accountModel.isValid;
+    else
       viewModelHelper.modelErrors = $scope.accountModel.errors;
-      if (viewModelHelper.modelIsValid) {
+  }
 
-      }
-    }
-
-    setupRules();
-  });
+  setupRules();
+});
---
+++ CarRental.Web/Controllers/API/AccountApiController.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Composition;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CarRental.Web.Core;
+using CarRental.Web.Models;
+
+namespace CarRental.Web.Controllers.API {
+  [Export]
+  [PartCreationPolicy(CreationPolicy.NonShared)]
+  [RoutePrefix("api/account")]
+  public class AccountApiController : ApiControllerBase {
+    private ISecurityAdapter _securityAdapter;
+
+    [ImportingConstructor]
+    public AccountApiController(ISecurityAdapter securityAdapter) {
+      _securityAdapter = securityAdapter;
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public HttpResponseMessage Login(HttpRequestMessage request, [FromBody]AccountLoginModel accountModel) {
+      return GetHttpResponse(request, () => {
+        HttpResponseMessage response;
+        var success = _securityAdapter.Login(accountModel.LoginEmail, accountModel.Password, accountModel.RememberMe);
+
+        if (success) {
+          response = request.CreateResponse(HttpStatusCode.OK);
+        }
+        else {
+          response = request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Unauthorized login.");
+        }
+
+        return response;
+      });
+    }
+  }
+}
